23e574a7d1640d24c2552d7503fd9ce2148704b2
AadithyaV <70337236+Aadithya-V@users.noreply.github.com>
2023-10-26 18:25:55 +0530

    refactor: use common code from parent package sql and rabbitmq connection and init uses consistent varialbles and code

--- imgcompressionservice/main.go
+++ imgcompressionservice/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 
-	"github.com/go-sql-driver/mysql"
-	amqp "github.com/rabbitmq/amqp091-go"
-
+	"github.com/Aadithya-V/mqimgstore/database"
 	"github.com/Aadithya-V/mqimgstore/imgcompressionservice/handlers"
+	"github.com/Aadithya-V/mqimgstore/queue"
 )
 
 // Constants specifying the listening addresses of
@@ -18,43 +15,13 @@ var (
 
 func main() {
 
-	db := initSqlDB()
+	db := database.NewMySQLSession()
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	msgQ := queue.NewMessageBroker()
+	defer msgQ.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	msgs, err := msgQ.Consume(queue.ImageCompressionQueue)
 
-	// Should be CONSISTENT WITH PRODUCER!!
-	q, err := ch.QueueDeclare(
-		"img-compression-queue", // name
-		true,                    // durable
-		false,                   // delete when unused
-		false,                   // exclusive
-		false,                   // no-wait
-		nil,                     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-
-	msgs, err := ch.Consume( // read consume's doc for ack mechanisms
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
 	failOnError(err, "Failed to register a consumer")
 
 	var forever chan struct{}
@@ -70,30 +37,6 @@ func main() {
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
-}
-
-func initSqlDB() *sql.DB {
-	// Capture connection properties.
-	cfg := mysql.Config{
-		User:   "go",  //os.Getenv("DBUSER"),
-		Passwd: "123", //os.Getenv("DBPASS"),
-		Net:    "tcp",
-		Addr:   MySQLAddr,
-		DBName: "test",
-	}
-	// Get a database handle.
-	db, err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
-	fmt.Println("MySQL Connected!")
-
-	return db
 }
 
 func failOnError(err error, msg string) {

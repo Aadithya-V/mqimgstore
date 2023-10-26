8402f7ade5aaed843821f4afa1bee6e891190e29
AadithyaV <70337236+Aadithya-V@users.noreply.github.com>
2023-10-26 18:44:40 +0530

    chore: scaffold design with file api

---
+++ imgcompressionservice/images/repository.go
@@ -0,0 +1,33 @@
+package images
+
+/* import "net/url"
+
+type Respository struct {
+	// file system handler, fd
+}
+
+func NewRepository() *Respository {
+	return &Respository{}
+}
+
+func (r *Respository) GetImageByURL(url string) ([]byte, error) {
+
+	// TODO: Parse url using net/url
+
+
+
+
+
+	return nil, nil
+}
+
+// NOT IMPLEMENTED FOR NOW
+func (r *Respository) StoreImageByURL(url string, image []byte) ([]byte, error) {
+	// Parse url using net/url
+
+	return nil, nil
+}
+
+func parseURL(url string) url.URL {
+	return nil
+} */
